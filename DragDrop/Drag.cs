499275a44af45023e0f0780ca0d5b26c6f3efc6e
AbanoubHM <abanoubharby33@gmail.com>
2022-02-13 02:20:37 +0200

    Add project files.

---
+++ DragDrop/Drag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragDrop {
+    public partial class Drag : Form {
+        Graphics g;
+        Rectangle rect = new Rectangle(50,50,50,50);
+        public Drag() {
+            InitializeComponent();
+        }
+        override protected void OnPaint(PaintEventArgs e) {
+            g = e.Graphics;
+            g.FillRectangle(new SolidBrush(Color.Red), rect);
+            base.OnPaint(e);
+        }
+        protected override void OnMouseDown(MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                rect = new Rectangle(e.X - 25, e.Y - 25, 50, 50);
+                Invalidate();
+            }
+        }
+        protected override void OnMouseMove(MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                rect = new Rectangle(e.X - 25, e.Y - 25, 50, 50);
+                Invalidate();
+            }
+        }
+
+    }
+}
---
+++ Draw/Draw.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw {
+    public partial class Draw : Form {
+        bool moving = false;
+        int x = -1;
+        int y = -1;
+        Graphics g;
+        public Draw() {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e) {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e) {
+            if (moving && x != -1 && y != -1) {
+                g.DrawLine(new Pen(Color.Red), new Point(x, y), e.Location);
+            }
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e) {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+    }
+}
